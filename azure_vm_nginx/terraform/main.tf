28023b52da70fe6017bef59d61435f08ad63f286
Gizachew Bayness <eleccrazy24@gmail.com>
2025-05-26 02:03:54 +0200

    Enable HTTPS: Update NSG for port 443 and configure NGINX for SSL and HTTP redirect

--- azure_vm_nginx/terraform/main.tf
+++ azure_vm_nginx/terraform/main.tf
@@ -90,6 +90,19 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+    security_rule {
+    name                       = "allow_https"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
   depends_on = [azurerm_resource_group.rg]
 }
 # Associate the network security group with the subnet
